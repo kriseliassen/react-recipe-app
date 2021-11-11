441846afab3e0a9dfefa9d9a8a9f57d234242aab
kriseliassen <kristin.eliassen@gmail.com>
2021-11-11 14:59:23 +0100

    Added suggestions to search component

--- client/src/components/Recipes.js
+++ client/src/components/Recipes.js
@@ -2,24 +2,12 @@ import React from 'react';
 import RecipeCard from './RecipeCard';
 
 const Recipes = ({recipes, searchTags, addLikedRecipe, removeLikedRecipe}) => {
-  let title;
-  let tags = searchTags.split(' ')
-  if (tags.length !== 0) {
-    const dinnerIndex = tags.findIndex(tag => tag === 'dinner');
-    const breakfastIndex = tags.findIndex(tag => tag === 'breakfast');
-    if (dinnerIndex >= 0 && dinnerIndex !== tags.length - 1) {
-      tags.splice(dinnerIndex, 1)
-      title = tags.join(', ');
-      title = `dinners: ${title}`;
-    } else if (breakfastIndex >= 0 && breakfastIndex !== tags.length - 1) {
-      tags.splice(breakfastIndex, 1)
-      title = tags.join(', ');
-      title = `breakfasts: ${title}`;
-    } else if (breakfastIndex >= 0) {
-      title = 'any breakfasts'
-    } else if (dinnerIndex >= 0) {
-      title = 'any dinners'
-    }
+  let title = searchTags;
+  if (title === 'dinner') {
+    title = 'any dinners'
+  }
+  if (title === 'breakfast') {
+    title = 'breakfast for dinner'
   }
 
   return (
--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 
 const Search = ({getRandomRecipe, getRecipes, setSearchTags}) => {
+  const [query, setQuery] = useState([])
   const [value, setValue] = useState('')
 
   const handleBtnClick = (meal) => {
@@ -12,10 +13,20 @@ const Search = ({getRandomRecipe, getRecipes, setSearchTags}) => {
     setValue(e.target.value);
   };
 
+  const toggleQuery = (e) => {
+    const string = e.target.textContent
+    if (query.includes(string)) {
+      const updatedArray = query.filter(item => item !== string)
+      setQuery(updatedArray)
+      return
+    }
+    setQuery([...query, string])
+  }
+
   const handleSubmit = async e => {
     e.preventDefault();
-    setSearchTags('dinner ' + value)
-    getRecipes('dinner ' + value)
+    setSearchTags(query.join(' '))
+    getRecipes(query)
     setValue('');
     e.target.query.blur();
   };
@@ -24,6 +35,14 @@ const Search = ({getRandomRecipe, getRecipes, setSearchTags}) => {
     <div>
       <h2>What do you want for dinner?</h2>
       <p>Search using your own keywords or click some of our suggestions</p>
+      <p onClick={toggleQuery}>healthy</p>
+      <p onClick={toggleQuery}>salad</p>
+      <p onClick={toggleQuery}>pasta</p>
+      <p onClick={toggleQuery}>soup</p>
+      <p onClick={toggleQuery}>potato</p>
+      <p onClick={toggleQuery}>quick</p>
+      <p onClick={toggleQuery}>vegetarian</p>
+      <p onClick={toggleQuery}>vegan</p>
       <form onSubmit={handleSubmit}>
         <label htmlFor="Form__input" className="Form__label">
         <input
@@ -32,9 +51,8 @@ const Search = ({getRandomRecipe, getRecipes, setSearchTags}) => {
             className="Form__input"
             autoComplete="off"
             name="query"
-            value={value}
+            value={value || query}
             required
-            list="queries"
             onChange={handleChange} />
         </label>
         <input type="submit" className="Form__btn" value="Get suggestions!" />
