a5714739eb3906ba8a28f5c54242f53a30edf6e1
kriseliassen <kristin.eliassen@gmail.com>
2021-11-12 11:48:59 +0100

    About page done

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -7,8 +7,7 @@ const About = () => {
     <div className="About__container">
       <h1 className="About__header">About 'What's for dinner?'</h1>
       <p className="About__text">
-      Jujubes macaroon liquorice muffin danish cake apple pie. Gingerbread candy canes donut cookie cake cake. Powder ice cream jelly topping wafer fruitcake topping. Pie brownie bonbon muffin sweet toffee chocolate.
-      Cotton candy lollipop sweet candy sesame snaps. Apple pie apple pie bear claw caramels cotton candy caramels pudding. Caramels marzipan chupa chups lemon drops wafer gingerbread toffee. Dessert danish ice cream sugar plum gingerbread chocolate bar tiramisu.
+        Setting the scene: You're heading home from work, tired and hungry. You want to cook something for dinner, but you don't want to make too many decisions. Open up your "What's for dinner?" and either enter a keyword for what you're in the mood for or choose one of the suggested keywords. Maybe you're so hungry you'll eat any dinner? There's a button for that. Or maybe you prefer breakfast foods, even for dinner? There's a button for that as well. <br/>Either way, you'll get three suggested recipes based on your input. Not too many, so it won't overwhelm you or make it hard to decide, but enough to give you a choice. None of the suggested recipes sound good? Try another keyword! Once you find recipes you like, click the heart and store them for next time you're wondering What's for dinner.
       </p>
       <a href="https://spoonacular.com/food-api"  className="About__link--api">Powered by Spoonacular API</a>
       <a href="https://github.com/kriseliassen" className="About__link--github"><BsGithub /></a>
