bb8a826575e74afd8bf074632d14f884d05fb85b
kriseliassen <kristin.eliassen@gmail.com>
2021-11-12 08:37:27 +0100

    Styling done for Recipe page

--- client/src/components/Recipe.js
+++ client/src/components/Recipe.js
@@ -1,22 +1,32 @@
 import React from 'react'
 import parser from 'html-react-parser';
+import '../styles/Recipe.css'
 
 const Recipe = ({recipe}) => {
   const instructions = recipe ? parser(recipe.instructions) : '';
-
+  const diets = recipe.diets.join(', ')
   return (
       <div className="Recipe__container">
         <div className="Recipe__flag--favourite">Favourite</div>
         <h1 className="Recipe__header">{recipe.title}</h1>
         <img src={recipe.image} alt="img" className="Recipe__image"/>
         <div className="Recipe__details">
-          <p className="Recipe__details--text RecipeCard__details-time">Time to make: {recipe.readyInMinutes} minutes</p>
-          <p className="Recipe__details--text Recipe__details--text-servings">Servings: {recipe.servings}</p>
-          <div className="Recipe__details--text Recipe__details--text-diets">Suitable for: {recipe.diets.map(diet => <p key={diet} className="Recipe__details--text Recipe__details--diet">{diet}</p>)}</div>
+          <p className="Recipe__details--text RecipeCard__details-time"><span className="text--bold">Time to make:</span> {recipe.readyInMinutes} minutes</p>
+          <p className="Recipe__details--text Recipe__details--text-servings"><span className="text--bold">Servings:</span> {recipe.servings}</p>
+          <div className="Recipe__text--details Recipe__diets"> {diets}
+          </div>
         </div>
         <div className="Recipe__text--container">
-          <div className="Recipe__text Recipe__ingredients">{recipe.extendedIngredients.map(item => <p key={item.id}className="Recipe__text Recipe__ingredient-item">{item.originalString}</p>)}</div>
-          <div className="Recipe__text Recipe__instructions">{instructions}</div>
+          <div className="Recipe__text Recipe__ingredients">
+            <h3 className="Recipe__ingredients--header">Ingredients</h3>
+            <div className="Recipe__ingredients--list">
+              {recipe.extendedIngredients.map(item => <p key={item.id}className="Recipe__text Recipe__ingredient-item">{item.originalString}</p>)}
+            </div>
+          </div>
+          <div className="Recipe__text Recipe__instructions">
+            <h3 className="Recipe__instructions--header">Instructions</h3>
+            <p className="Recipe__instructions--text">{instructions}</p>
+          </div>
         </div>
       </div>
   )
