196151648e8b2dc8743fdd08e82e3f7ac341bb58
Kristin Eliassen <kristin.eliassen@gmail.com>
2022-01-11 15:59:30 +0100

    Fixed bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,85 +1,132 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable jsx-a11y/alt-text */
-import React, { useState, useEffect } from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
-import Navbar from './components/Navbar'
-import LikedRecipes from './components/LikedRecipes';
-import About from './components/About';
-import Search from './components/Search'
-import Recipe from './components/Recipe'
-import Recipes from './components/Recipes';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import LikedRecipes from "./components/LikedRecipes";
+import About from "./components/About";
+import Search from "./components/Search";
+import Recipe from "./components/Recipe";
+import Recipes from "./components/Recipes";
+import "./App.css";
 
 function App() {
-  const [recipes, setRecipes] = useState(null);
-  const [searchTags, setSearchTags] = useState(null)
-  const [clickedRecipe, setClickedRecipe] = useState(null);
-  const [likedRecipes, setLikedRecipes] = useState([])
-  
-  const navigate = useNavigate();
+	const [recipes, setRecipes] = useState([]);
+	const [searchTags, setSearchTags] = useState(null);
+	const [clickedRecipe, setClickedRecipe] = useState(null);
+	const [likedRecipes, setLikedRecipes] = useState([]);
 
-  useEffect(() => {
-    if (clickedRecipe === null) {
-      return
-    }
-    navigate(`/recipes/${clickedRecipe.title}`);
-  }, [clickedRecipe])
+	const navigate = useNavigate();
 
-  useEffect(() => {
-    const localStorageData = localStorage.getItem('likedRecipes');
-    setLikedRecipes(localStorageData ? JSON.parse(localStorageData) : []);
-  }, []);
-  
-  useEffect(() => {
-    localStorage.setItem('likedRecipes', JSON.stringify(likedRecipes));
-  }, [likedRecipes]);
+	useEffect(() => {
+		if (clickedRecipe === null) {
+			return;
+		}
+		navigate(`/recipes/${clickedRecipe.title}`);
+	}, [clickedRecipe]);
 
-  const fetchData = async (tags) => {
-    const path = `/api/random/${tags}`
-    await fetch(path)
-      .then(res => res.json())
-      .then(data => setRecipes(data));
-    return
-  }
+	useEffect(() => {
+		const localStorageData = localStorage.getItem("likedRecipes");
+		setLikedRecipes(localStorageData ? JSON.parse(localStorageData) : []);
+	}, []);
 
-  const getRecipes = async (tags) => {
-    await fetchData(tags);
-    navigate('/recipes')
-  };
+	useEffect(() => {
+		localStorage.setItem("likedRecipes", JSON.stringify(likedRecipes));
+	}, [likedRecipes]);
 
-  const getRandomRecipe = async (tags) => {
-    await fetchData(tags);
-    navigate('/recipes')
-  }
+	const fetchData = async (tags) => {
+		const path = `/api/random/${tags}`;
+		await fetch(path)
+			.then((res) => res.json())
+			.then((data) => {
+				setRecipes(data.recipes);
+			});
+		return;
+	};
 
-  const addLikedRecipe = (recipe) => {
-    setLikedRecipes([...likedRecipes, recipe]);
-  }
+	const getRecipes = async (tags) => {
+		await fetchData(tags);
+		navigate("/recipes");
+		return;
+	};
 
-  const removeLikedRecipe = (recipe) => {
-    const updatedList = likedRecipes.filter(item => item.id !== recipe.id)
-    setLikedRecipes(updatedList);
-    const recipeInState = recipes.find(item => item.id === recipe.id)
-    const updatedRecipe = {...recipeInState, isLiked: false}
-    const updatedState = recipes.map(item => item.id === updatedRecipe.id ? updatedRecipe : item)
-    setRecipes(updatedState)
-  }
+	const getRandomRecipe = async (tags) => {
+		await fetchData(tags);
+		navigate("/recipes");
+		return;
+	};
 
-  return (
-    <div className="App__container">
-        <div className="App__navbar--container">
-          <p className="App__navbar--logo">What's for dinner?</p>
-          <Navbar recipes={recipes}/>
-        </div>
-        <Routes>
-          <Route path="/" element={<Search getRandomRecipe={getRandomRecipe} getRecipes={getRecipes} searchTags={searchTags} setSearchTags={setSearchTags}/>}/>
-          <Route path="/recipes" element={<Recipes recipes={recipes} setClickedRecipe={setClickedRecipe} searchTags={searchTags} addLikedRecipe={addLikedRecipe} removeLikedRecipe={removeLikedRecipe}/>}/>
-          <Route path="/recipes/:title" element={<Recipe recipe={clickedRecipe} addLikedRecipe={addLikedRecipe} removeLikedRecipe={removeLikedRecipe}/>}/>
-          <Route path="/favourites" element={<LikedRecipes likedRecipes={likedRecipes} addLikedRecipe={addLikedRecipe} setClickedRecipe={setClickedRecipe} removeLikedRecipe={removeLikedRecipe}/>}/>
-          <Route path="/about" element={<About />}/>
-        </Routes>
-    </div>
-  );
+	const addLikedRecipe = (recipe) => {
+		setLikedRecipes([...likedRecipes, recipe]);
+		return;
+	};
+
+	const removeLikedRecipe = (recipe) => {
+		const updatedList = likedRecipes.filter((item) => item.id !== recipe.id);
+		setLikedRecipes(updatedList);
+		const recipeInState = recipes.find((item) => item.id === recipe.id);
+		const updatedRecipe = { ...recipeInState, isLiked: false };
+		const updatedState = recipes.map((item) =>
+			item.id === updatedRecipe.id ? updatedRecipe : item
+		);
+		setRecipes(updatedState);
+	};
+
+	return (
+		<div className="App__container">
+			<div className="App__navbar--container">
+				<p className="App__navbar--logo">What's for dinner?</p>
+				<Navbar recipes={recipes} />
+			</div>
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<Search
+							getRandomRecipe={getRandomRecipe}
+							getRecipes={getRecipes}
+							searchTags={searchTags}
+							setSearchTags={setSearchTags}
+						/>
+					}
+				/>
+				<Route
+					path="/recipes"
+					element={
+						<Recipes
+							recipes={recipes}
+							setClickedRecipe={setClickedRecipe}
+							searchTags={searchTags}
+							addLikedRecipe={addLikedRecipe}
+							removeLikedRecipe={removeLikedRecipe}
+						/>
+					}
+				/>
+				<Route
+					path="/recipes/:title"
+					element={
+						<Recipe
+							recipe={clickedRecipe}
+							addLikedRecipe={addLikedRecipe}
+							removeLikedRecipe={removeLikedRecipe}
+						/>
+					}
+				/>
+				<Route
+					path="/favourites"
+					element={
+						<LikedRecipes
+							likedRecipes={likedRecipes}
+							addLikedRecipe={addLikedRecipe}
+							setClickedRecipe={setClickedRecipe}
+							removeLikedRecipe={removeLikedRecipe}
+						/>
+					}
+				/>
+				<Route path="/about" element={<About />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
--- client/src/components/Recipes.js
+++ client/src/components/Recipes.js
@@ -1,35 +1,62 @@
-import React from 'react';
-import RecipeCard from './RecipeCard';
-import { Link } from 'react-router-dom';
-import '../styles/Recipes.css';
+import React from "react";
+import RecipeCard from "./RecipeCard";
+import { Link } from "react-router-dom";
+import "../styles/Recipes.css";
 
-const Recipes = ({recipes, searchTags, addLikedRecipe, removeLikedRecipe, setClickedRecipe}) => {
-  let title = searchTags;
-  if (title === 'dinner') {
-    title = 'any dinners'
-  }
-  if (title === 'breakfast') {
-    title = 'breakfast for dinner'
-  }
+const Recipes = ({
+	recipes,
+	searchTags,
+	addLikedRecipe,
+	removeLikedRecipe,
+	setClickedRecipe,
+}) => {
+	let title = searchTags;
+	if (title === "dinner") {
+		title = "any dinners";
+	}
+	if (title === "breakfast") {
+		title = "breakfast for dinner";
+	}
 
-  if (recipes.length === 0){
-    return <div className="Recipes__container">
-      <h1 className="Recipes__header">Recipes</h1>
-      <div className="Recipes__empty">
-        <p className="Recipes__empty-text">Oh no, we have no suggested recipes for this keyword right now. Try something else!</p>
-        <p className="Recipes__empty-btn"><Link to="/">Go back to search</Link></p>
-      </div>
-    </div>
-  }
-  
-  if (recipes) {
-    return <div className="Recipes__container">
-        <h1 className="Recipes__header">Recipes for<span className="Recipes__header--search-term">{title}</span></h1>
-        {recipes.map(recipe => <RecipeCard recipe={recipe} key={recipe.id} setClickedRecipe={setClickedRecipe}  addLikedRecipe={addLikedRecipe} removeLikedRecipe={removeLikedRecipe}/>)}
-        <Link to="/" className="Recipes__btn-goBack">Try another keyword</Link>
-  </div>
-  }
-  
-}
+	if (recipes.length === 0) {
+		return (
+			<div className="Recipes__container">
+				<h1 className="Recipes__header">Recipes</h1>
+				<div className="Recipes__empty">
+					<p className="Recipes__empty-text">
+						Oh no, we have no suggested recipes for this keyword right now. Try
+						something else!
+					</p>
+					<p className="Recipes__empty-btn">
+						<Link to="/">Go back to search</Link>
+					</p>
+				</div>
+			</div>
+		);
+	}
 
-export default Recipes
+	if (recipes) {
+		return (
+			<div className="Recipes__container">
+				<h1 className="Recipes__header">
+					Recipes for
+					<span className="Recipes__header--search-term">{title}</span>
+				</h1>
+				{recipes.map((recipe) => (
+					<RecipeCard
+						recipe={recipe}
+						key={recipe.id}
+						setClickedRecipe={setClickedRecipe}
+						addLikedRecipe={addLikedRecipe}
+						removeLikedRecipe={removeLikedRecipe}
+					/>
+				))}
+				<Link to="/" className="Recipes__btn-goBack">
+					Try another keyword
+				</Link>
+			</div>
+		);
+	}
+};
+
+export default Recipes;
