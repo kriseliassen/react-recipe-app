7b4964fb66da585dcef1a62be541fdf28b955f4d
Kristin Eliassen <kristin.eliassen@gmail.com>
2022-02-10 15:50:33 +0100

    MR

--- client/src/components/LikedRecipes.js
+++ client/src/components/LikedRecipes.js
@@ -1,15 +1,34 @@
-import React from 'react'
-import RecipeCard from './RecipeCard'
-import '../styles/LikedRecipes.css'
+import React from "react";
+import RecipeCard from "./RecipeCard";
+import "../styles/LikedRecipes.css";
 
-const LikedRecipes = ({ likedRecipes, setClickedRecipe, addLikedRecipe, removeLikedRecipe }) => {
-  return (
-    <div className="Liked__container">
-      <h1 className="Liked__header">Your favourite recipes</h1>
-      {likedRecipes.length === 0 && <p className="Liked__text--empty">You have no favourite recipes yet. Try searching for some new recipes first and click the heart on the ones you want to save!</p>}
-      {likedRecipes && likedRecipes.map(recipe => <RecipeCard recipe={recipe} key={recipe.image} setClickedRecipe={setClickedRecipe}  addLikedRecipe={addLikedRecipe} removeLikedRecipe={removeLikedRecipe}/>)}
-    </div>
-  )
-}
+const LikedRecipes = ({
+	likedRecipes,
+	setClickedRecipe,
+	addLikedRecipe,
+	removeLikedRecipe,
+}) => {
+	return (
+		<div className="Liked__container">
+			<h1 className="Liked__header">Your favourite recipes</h1>
+			{likedRecipes.length === 0 && (
+				<p className="Liked__text--empty">
+					You have no favourite recipes yet. Try searching for some new recipes
+					first and click the upper-right corner to save the ones you like!
+				</p>
+			)}
+			{likedRecipes &&
+				likedRecipes.map((recipe) => (
+					<RecipeCard
+						recipe={recipe}
+						key={recipe.image}
+						setClickedRecipe={setClickedRecipe}
+						addLikedRecipe={addLikedRecipe}
+						removeLikedRecipe={removeLikedRecipe}
+					/>
+				))}
+		</div>
+	);
+};
 
-export default LikedRecipes
+export default LikedRecipes;
