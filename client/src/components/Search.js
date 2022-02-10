f648dd06ba21936bf8e1f6b798101d2ba96136ed
Kristin Eliassen <kristin.eliassen@gmail.com>
2022-02-10 13:34:16 +0100

    Fixed bug where search function didn't work

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,69 +1,106 @@
-import React, { useState } from 'react'
-import '../styles/Search.css'
+import React, { useState } from "react";
+import "../styles/Search.css";
 
-const Search = ({getRandomRecipe, getRecipes, setSearchTags}) => {
-  // const [query, setQuery] = useState([])
-  const [value, setValue] = useState('')
-  const [activeItemId, setActiveItemId] = useState(null);
+const Search = ({ getRandomRecipe, getRecipes, setSearchTags }) => {
+	// const [query, setQuery] = useState([])
+	const [value, setValue] = useState("");
+	const [activeItemId, setActiveItemId] = useState(null);
 
-  const handleBtnClick = (meal) => {
-    setSearchTags(meal)
-    getRandomRecipe(meal);
-  }
+	const handleBtnClick = (meal) => {
+		setSearchTags(meal);
+		getRandomRecipe(meal);
+	};
 
-  const handleChange = e => {
-    setValue(e.target.value);
-  };
+	const handleChange = (e) => {
+		setValue(e.target.value);
+	};
 
-  const handleSubmit = async e => {
-    const searchTerm = [value]
-    e.preventDefault();
-    setSearchTags(searchTerm + '')
-    getRecipes([...searchTerm, 'dinner'])
-    setValue('');
-    e.target.query.blur();
-  };
+	const handleSubmit = async (e) => {
+		const searchTerm = [value];
+		console.log("searchTerm: ", searchTerm);
+		e.preventDefault();
+		setSearchTags(searchTerm + "");
+		getRecipes([...searchTerm, "dinner"]);
+		setValue("");
+		e.target.query.blur();
+	};
 
-  const toggleActive = (id) => {
-    if(activeItemId === id) {
-      setActiveItemId(null);
-      setValue('')
-      return;
-    }
-    setActiveItemId(id);
-    setValue(id)
-    // setQuery([id])
-  };
+	const toggleActive = (id) => {
+		if (activeItemId === id) {
+			setActiveItemId(null);
+			setValue("");
+			return;
+		}
+		setActiveItemId(id);
+		setValue(id);
+		// setQuery([id])
+	};
 
-  const searchSuggestions = ['salad', 'pasta', 'soup', 'potato', 'quick', 'vegetarian', 'vegan', 'salmon']
+	const searchSuggestions = [
+		"salad",
+		"pasta",
+		"soup",
+		"potato",
+		"quick",
+		"vegetarian",
+		"vegan",
+		"salmon",
+	];
 
-  return (
-    <div className="Search__container">
-      <h2 className="Search__header">What are you in the mood for?</h2>
-      <p className="Search__subheader">Search using your own keyword or click one of our suggestions</p>
-      <div className="Search__suggestion--list">
-        {searchSuggestions.map(item => <p className={`Search__suggestion ${activeItemId === item ? "active" : null}`} key={item} onClick={() => toggleActive(item)}>{item}</p>)}
-      </div>
-      <form onSubmit={handleSubmit} className="Search__form">
-        <label htmlFor="Form__input" className="Form__label">
-          <input
-              type="text"
-              id="Form__input"
-              className="Form__input"
-              autoComplete="off"
-              name="query"
-              value={value}
-              required
-              onChange={handleChange}/>
-        </label>
-        <input type="submit" className="btn Form__btn" value="Get suggestions!" />
-      </form>
-      <div className="Search__random-btns-container">
-        <button onClick={() => handleBtnClick('dinner')} className="btn Search__btn Search__btn--dinner">I'm not picky, suggest any dinner</button>
-        <button onClick={() => handleBtnClick('breakfast')} className="btn Search__btn Search__btn--dinner">I want breakfast for dinner</button>
-      </div>
-    </div>
-  )
-}
+	return (
+		<div className="Search__container">
+			<h2 className="Search__header">What are you in the mood for?</h2>
+			<p className="Search__subheader">
+				Search using your own keyword or click one of our suggestions
+			</p>
+			<div className="Search__suggestion--list">
+				{searchSuggestions.map((item) => (
+					<p
+						className={`Search__suggestion ${
+							activeItemId === item ? "active" : null
+						}`}
+						key={item}
+						onClick={() => toggleActive(item)}
+					>
+						{item}
+					</p>
+				))}
+			</div>
+			<form onSubmit={handleSubmit} className="Search__form">
+				<label htmlFor="Form__input" className="Form__label">
+					<input
+						type="text"
+						id="Form__input"
+						className="Form__input"
+						autoComplete="off"
+						name="query"
+						value={value}
+						required
+						onChange={handleChange}
+					/>
+				</label>
+				<input
+					type="submit"
+					className="btn Form__btn"
+					value="Get suggestions!"
+				/>
+			</form>
+			<div className="Search__random-btns-container">
+				<button
+					onClick={() => handleBtnClick("dinner")}
+					className="btn Search__btn Search__btn--dinner"
+				>
+					I'm not picky, suggest any dinner
+				</button>
+				<button
+					onClick={() => handleBtnClick("breakfast")}
+					className="btn Search__btn Search__btn--dinner"
+				>
+					I want breakfast for dinner
+				</button>
+			</div>
+		</div>
+	);
+};
 
-export default Search
+export default Search;
--- server/index.js
+++ server/index.js
@@ -1,46 +1,55 @@
-import express from 'express'
-import dotenv  from "dotenv"
-import axios from 'axios';
-import fs, { read, readFile } from 'fs';
-import { promisify } from 'util';
-import { v4 as uuidv4 } from 'uuid';
+import express from "express";
+import dotenv from "dotenv";
+import axios from "axios";
+import fs, { read, readFile } from "fs";
+import { promisify } from "util";
+import { v4 as uuidv4 } from "uuid";
 
-dotenv.config()
+dotenv.config();
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-const apiKey = process.env.SPOONACULAR_API_KEY
-const apiUrl = `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}`
+const apiKey = process.env.SPOONACULAR_API_KEY;
+const apiUrl = `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}`;
 
 const readFilePromise = promisify(fs.readFile);
 const writeFilePromise = promisify(fs.writeFile);
 
 const readDatabase = async (path) => {
-  const data = await readFilePromise(path, 'utf-8')
-  console.log('read db', data)
-  return data;
-}
+	const data = await readFilePromise(path, "utf-8");
+	console.log("read db", data);
+	return data;
+};
 
 const writeToDb = async (path, content) => {
-  await writeFilePromise(path, JSON.stringify(content));
+	await writeFilePromise(path, JSON.stringify(content));
 };
+
+// GET DINNERS BY SEARCH TERM
+app.get("/api/random/:tags", async (req, res) => {
+	const { tags } = req.params;
+	console.log("tags: ", tags);
+	const url = `${apiUrl}&number=3&tags=${tags}`;
+	const response = await axios.get(url);
+	res.json(response.data);
+});
 
 // GET RANDOM DINNERS
 app.get("/api/random/dinner", async (_, res) => {
-  const url = `${apiUrl}&number=3&tags=dinner`
-  const response = await axios.get(url)
-  res.json(response.data);
-})
+	const url = `${apiUrl}&number=3&tags=dinner`;
+	const response = await axios.get(url);
+	res.json(response.data);
+});
 
-// GET RANDOM BREAKFASTS 
+// GET RANDOM BREAKFASTS
 app.get("/api/random/breakfast", async (_, res) => {
-  const url = `${apiUrl}&number=3&tags=breakfast`
-  const response = await axios.get(url)
-  res.json(response.data);
-})
+	const url = `${apiUrl}&number=3&tags=breakfast`;
+	const response = await axios.get(url);
+	res.json(response.data);
+});
 
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+	console.log(`Server listening on ${PORT}`);
 });
